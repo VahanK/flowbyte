3949fbcf964bb67093b083ebbb84aae921e01ed0
VahanK <kupelian11@gmail.com>
2026-04-28 18:18:53 +0300

    fix: wire real WhatsApp number 96171180871

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const WHATSAPP_NUMBER = "96170123456"
+export const WHATSAPP_NUMBER = "96171180871"
 
 export function whatsappLink(message: string) {
   return `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(message)}`
